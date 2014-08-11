8da1c14c2aa2ba6f849218b4a2b3e93553241bc6
Gonzalo Barco <gbarcouy@gmail.com>
2014-08-11 17:17:23 -0300

    Stub WebService::Amazon::SimpleDB::Server.

---
+++ lib/WebService/Amazon/SimpleDB/Server.pm
@@ -0,0 +1,108 @@
+package WebService::Amazon::SimpleDB::Server;
+
+use v5.10.0;
+use strict;
+use warnings FATAL => 'all';
+use Dancer;
+
+=head1 NAME
+
+WebService::Amazon::SimpleDB::Server - Opensource RESTful server aiming at Amazon SimpleDB API 2009-04-15 compatibility.
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WebService::Amazon::SimpleDB::Server;
+
+    my $foo = WebService::Amazon::SimpleDB::Server->new();
+    ...
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Gonzalo Barco, C<< <gbarcouy at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-webservice-amazon-simpledb-server at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WebService-Amazon-SimpleDB-Server>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WebService::Amazon::SimpleDB::Server
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WebService-Amazon-SimpleDB-Server>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WebService-Amazon-SimpleDB-Server>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WebService-Amazon-SimpleDB-Server>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WebService-Amazon-SimpleDB-Server/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Jean Pierre Ducassou L<search.cpan.org/~jpduca/> for the theoretical backgound,
+infinite patience and support on writing this module.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2014 Gonzalo Barco
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+=cut
+
+1; # End of WebService::Amazon::SimpleDB::Server
